cedb79132c08c0f2e35ccaafbef1e58b89ede41a
Marnay1 <87884789+Marnay1@users.noreply.github.com>
2021-12-22 18:03:19 -0500

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -2,11 +2,4 @@ function addUser(){
     username= document.getElementById("user_name").value;
     localStorage.setItem("user_name", username);
     window.location= "kwitter_room.html";
-    user_name= document.getElementById("user_name").value;
-    firebase.database().ref("/").child(user_name).update({
-        purpose : "adding user"
-    });
-}
-function check(){
-    console.log("works");
 }
